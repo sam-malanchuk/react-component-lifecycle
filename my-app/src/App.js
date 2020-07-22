9283de19a58292f4fc458ec27d30c92e9e6f8e36
Sam Malanchuk <malanchuksam@gmail.com>
2020-07-22 19:49:25 -0400

    added the mounting functionality of the Counter component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,24 @@ import React from 'react';
 import Counter from './Counter.js';
 import './App.css';
 
-function App() {
-  return <div>
-    <Counter />
-  </div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mount: true
+    }
+
+    this.mountCounter = () => this.setState({mount: true});
+    this.unmountCounter = () => this.setState({mount: false});
+  }
+  render() {
+    return <div>
+      <button onClick={this.mountCounter} disabled={this.state.mount}>Mount</button>
+      <button onClick={this.unmountCounter} disabled={!this.state.mount}>Unmount</button>
+      {this.state.mount ? <Counter /> : null}
+    </div>
+  }
 }
 
 export default App;
