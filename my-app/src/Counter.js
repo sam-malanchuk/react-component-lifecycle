42f28c7d6e77d5eee2f686f0b535d86a007e744d
Sam Malanchuk <malanchuksam@gmail.com>
2020-07-21 21:50:01 -0400

    added a way to increment and decrement the counter value

--- my-app/src/Counter.js
+++ my-app/src/Counter.js
@@ -7,11 +7,16 @@ export default class Counter extends React.Component {
         this.state = {
             counter: 0
         }
+
+        this.increment = () => this.setState({counter: this.state.counter + 1})
+        this.decrement = () => this.setState({counter: this.state.counter - 1})
     }
     render () {
         console.log('Render');
 
         return <div>
+            <button onClick={this.increment}>Increment</button>
+            <button onClick={this.decrement}>Decrement</button>
             <div className="counter">
                 Counter: {this.state.counter}
             </div>
